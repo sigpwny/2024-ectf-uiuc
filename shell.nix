fa9566a8a70fcd9c9c3cacb3a35fcabccc5fe900
Minh Duong <github@whitehoodhacker.net>
2024-02-04 16:49:49 -0600

    Add msdk as git submodule (#16)
    * Add msdk as submodule
    
    * Add .gitmodules and msdk gitlink
    
    * Remove msdk git fetch from shell.nix

--- shell.nix
+++ shell.nix
@@ -29,13 +29,8 @@ pkgs.mkShell {
     pkgs.minicom
   ];
 
-  msdk = builtins.fetchGit {
-    url = "https://github.com/Analog-Devices-MSDK/msdk.git";
-    ref = "refs/tags/v2023_06";
-  };
   shellHook =
     ''
-      cp -r $msdk $PWD/msdk
       chmod -R u+rwX,go+rX,go-w $PWD/msdk
       export MAXIM_PATH=$PWD/msdk
     '';
