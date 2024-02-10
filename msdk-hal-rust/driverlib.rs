1890245c70d53ecfc8677ddd50a7d3b5cf5538d5
Minh Duong <git@whitehoodhacker.net>
2024-02-10 14:25:41 -0600

    wip: attempt to build minimal MSDK object file

--- msdk-hal-rust/driverlib.rs
+++ msdk-hal-rust/driverlib.rs
@@ -1,14 +1,21 @@
 mod driverwrapper {
   #[link(name = "driverwrapper")]
   extern "C" {
-      pub(super) fn init_system();
+    pub(super) fn init_system();
+    pub(super) fn led_on(idx: u32);
   }
 }
 
 pub fn init_system() {
   unsafe {
-      driverwrapper::init_system();
+    driverwrapper::init_system();
   }
 }
 
 // TODO: Add more peripherals
+
+pub fn led_on(idx: u32) {
+  unsafe {
+    driverwrapper::led_on(idx);
+  }
+}
