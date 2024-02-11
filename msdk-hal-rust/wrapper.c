229e962d6b47045150c663f9dea91a5bd8da63e7
jasmehar <jasmehar03kochhar@gmail.com>
2024-02-11 15:34:01 -0600

    added some standard UART elements

--- msdk-hal-rust/wrapper.c
+++ msdk-hal-rust/wrapper.c
@@ -5,6 +5,9 @@
 
 #include "led.h"
 #include "trng.h"
+#include "uart.h"
+
+#define UART ((uint32_t)UART0_BASE) //dont know if this is correct got it from 2023
 
 /**
  * @brief Initialize the system.
@@ -14,5 +17,13 @@
 void init_system(void) {
   // TODO: Add any peripheral initialization here
 }
-
+// baudrate=115200,
+// parity=serial.PARITY_NONE,
+// stopbits=serial.STOPBITS_ONE,
+// bytesize=serial.EIGHTBITS,
 // TODO: Add any other wrapper functions here
+static void uart_init(void){
+MXC_UART_Init(UART, 115200, mxc_uart_clock_t clock);
+MXC_UART_SetParity(UART, MXC_UART_PARITY_DISABLE);
+MXC_UART_SetStopBits(UART, MXC_UART_STOP_1);
+}
