ca67ac0b739960a09f8695514f4c241d790dc58d
jasmehar <jasmehar03kochhar@gmail.com>
2024-02-11 18:47:01 -0600

    using UART0

--- msdk-hal-rust/wrapper.c
+++ msdk-hal-rust/wrapper.c
@@ -7,7 +7,7 @@
 #include "trng.h"
 #include "uart.h"
 
-#define UART ((uint32_t)UART0_BASE) //dont know if this is correct got it from 2023
+mxc_uart_regs_t* UART ; //defined UART
 
 /**
  * @brief Initialize the system.
@@ -23,7 +23,7 @@ void init_system(void) {
 // bytesize=serial.EIGHTBITS,
 // TODO: Add any other wrapper functions here
 static void uart_init(void){
-MXC_UART_Init(UART, 115200, mxc_uart_clock_t clock);
-MXC_UART_SetParity(UART, MXC_UART_PARITY_DISABLE);
-MXC_UART_SetStopBits(UART, MXC_UART_STOP_1);
+  MXC_UART_Init(UART, 115200, mxc_uart_clock_t clock); //dont know the clock here
+  MXC_UART_SetParity(UART, MXC_UART_PARITY_DISABLE);
+  MXC_UART_SetStopBits(UART, MXC_UART_STOP_1);
 }
