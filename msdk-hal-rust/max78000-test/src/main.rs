12560cf71586291e8e43f226243281959627db8c
Minh Duong <git@whitehoodhacker.net>
2024-02-12 23:58:26 -0600

    Re-create SVD for accurate IVT

--- msdk-hal-rust/max78000-test/src/main.rs
+++ msdk-hal-rust/max78000-test/src/main.rs
@@ -2,8 +2,14 @@
 #![no_main]
 
 use panic_halt as _;
+// use panic_semihosting as _;
 use cortex_m_rt::entry;
 use max78000_pac as _;
+
+// pub union Vector {
+//     handler: unsafe extern "C" fn(),
+//     reserved: usize,
+// }
 
 #[entry]
 fn main() -> ! {
@@ -13,3 +19,17 @@ fn main() -> ! {
         continue;
     }
 }
+
+// #[link_section = ".vector_table.reset_vector"]
+// #[no_mangle]
+// static RESET_VECTOR: unsafe extern "C" fn() -> ! = main;
+
+// #[allow(unsafe_code)]
+// #[link_section = ".vector_table.exceptions"]
+// #[no_mangle]
+// static __EXCEPTIONS: [Option<unsafe extern "C" fn()>; 14] = [None; 14];
+
+// #[allow(unsafe_code)]
+// #[link_section = ".vector_table.interrupts"]
+// #[no_mangle]
+// static __INTERRUPTS: [Option<unsafe extern "C" fn()>; 104] = [None; 104];
