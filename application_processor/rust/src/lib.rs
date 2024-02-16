2a32508ca02b9f0d8e32146f105e1cc375c2324f
Minh Duong <github@whitehoodhacker.net>
2024-02-16 07:40:31 -0600

    Bootstrap Rust code for AP, Component, and MSDK HAL (#30)
    * Bootstrap Rust code for AP and MSDK HAL
    
    * Add sample Rust code
    
    * Add comment about prev year

---
+++ application_processor/rust/src/lib.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+
+fn secure_send(message: &[u8; 64]) {
+    // TODO
+}
+
+fn secure_recv(message: &mut [u8; 64]) {
+    // TODO
+}
+
+// Delays the MCU by the number of microseconds provided (blocking)
+fn delay(us: u32) {
+    // TODO
+}
+
+// Do not use this function in main.rs! Instead, use the formatted message functions
+fn host_uart_write(message: &[u8; 128]) {
+    // TODO
+}
+
+fn send_host_info(message: &[u8; 64]) {
+    // TODO
+}
+
+fn send_host_ack(message: &[u8; 64]) {
+    // TODO
+}
+
+fn send_host_success(message: &[u8; 64]) {
+    // TODO
+}
+
+fn send_host_error(message: &[u8; 64]) {
+    // TODO
+}
+
+fn send_host_debug(message: &[u8; 64]) {
+    // TODO
+}
+
+fn recv_host(message: &mut [u8; 32]) {
+    // TODO
+}
