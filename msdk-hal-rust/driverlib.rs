95b380c28cdf088e4bb0e2e71295706ed87113cf
jasmehar <jasmehar03kochhar@gmail.com>
2024-02-18 13:21:33 -0600

    not sure what the value for full is

--- msdk-hal-rust/driverlib.rs
+++ msdk-hal-rust/driverlib.rs
@@ -12,3 +12,14 @@ pub fn init_system() {
 }
 
 // TODO: Add more peripherals
+
+//write to trasmit fifo (use of u8 becasue unsigned char)
+//returns num bytes written
+pub fn UART_write_FIFO(bytes: &mut[u8], len: u32) -> u32 {
+  driverwrapper::UART_write_FIFO(*bytes, len);
+}
+//read from reciever fifo 
+//returns num bytes read
+pub fn UART_read_FIFO(bytes: &mut[u8], len: u32) -> u32 {
+  let ret: u32 = unsafe{driverwrappper::UART_read_FIFO(*bytes, len)};
+}
--- msdk-hal-rust/wrapper.c
+++ msdk-hal-rust/wrapper.c
@@ -7,7 +7,7 @@
 #include "trng.h"
 #include "uart.h"
 
-mxc_uart_regs_t* UART ; //defined UART
+
 
 /**
  * @brief Initialize the system.
@@ -16,6 +16,7 @@ mxc_uart_regs_t* UART ; //defined UART
  */
 void init_system(void) {
   // TODO: Add any peripheral initialization here
+  uart_init();
 }
 // baudrate=115200,
 // parity=serial.PARITY_NONE,
@@ -23,7 +24,33 @@ void init_system(void) {
 // bytesize=serial.EIGHTBITS,
 // TODO: Add any other wrapper functions here
 static void uart_init(void){
-  MXC_UART_Init(UART, 115200, mxc_uart_clock_t clock); //dont know the clock here
-  MXC_UART_SetParity(UART, MXC_UART_PARITY_DISABLE);
-  MXC_UART_SetStopBits(UART, MXC_UART_STOP_1);
+
+  MXC_UART_Init(MXC_UART0, 115200, MXC_UART_APB_CLK); //dont know the clock here
+  MXC_UART_SetParity(MXC_UART0, MXC_UART_PARITY_DISABLE);
+  MXC_UART_SetStopBits(MXC_UART0, MXC_UART_STOP_1);
+
 }
+
+//UART SEND POINTER AND SIZE
+unsigned int UART_write_FIFO(const unsigned char *bytes, unsigned int len){
+
+  if (MXC_UART_GetTXFIFOAvailable(MXC_UART0) < len){ //Get the amount of free space available in the transmit FIFO.
+    return -1;
+  }
+  return MXC_UART_WriteTXFIFO(MXC_UART0, *bytes, len); //return num bytes written
+
+} 
+//UART RECIEVE LENGTH DATA
+unsigned int UART_read_FIFO(unsigned char *bytes, unsigned int len){
+  
+  if (MXC_UART_GetRXFIFOAvailable(MXC_UART0) == FULL){ //Get the number of bytes currently available in the receive FIFO.
+    return -1;
+  }
+  return MXC_UART_ReadRXFIFO(MXC_UART0, *bytes, len); //return num bytes read
+
+}
+
+ 
+
+
+
