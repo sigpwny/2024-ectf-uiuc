ecd828596aeb56cfc79a010b9faefc52cd82e2e3
Minh Duong <git@whitehoodhacker.net>
2024-02-23 03:21:44 -0600

    Add crypto example

--- msdk-hal-rust/max78000-test/src/main.rs
+++ msdk-hal-rust/max78000-test/src/main.rs
@@ -4,6 +4,7 @@
 use cortex_m_rt::entry;
 use max78000_hal::tmr0;
 use board::{Board, Led};
+use board::secure_comms as hide;
 
 #[entry]
 fn main() -> ! {
@@ -11,6 +12,9 @@ fn main() -> ! {
     board.send_host_debug(b"Board initialized!");
 
     let mut count = 0;
+
+    test_ascon(&board);
+
     loop {
         // This timer logic doesn't make any sense, don't use it
         let tmr_cnt = tmr0::get_tick_count(&board.tmr0);
@@ -27,5 +31,52 @@ fn main() -> ! {
         }
         count += 1;
         continue;
+    }
+}
+
+fn test_ascon(board: &Board) {
+    let message: [u8; hide::LEN_ASCON_128_PTXT] = [65u8; hide::LEN_ASCON_128_PTXT];
+    let associated_data: [u8; hide::LEN_ASCON_128_AD] = [1, 2, 3, 4, 5, 6, 7, 8];
+    let nonce: [u8; hide::LEN_ASCON_128_NONCE] = [7u8; hide::LEN_ASCON_128_NONCE];
+    let key: [u8; hide::LEN_ASCON_128_KEY] = [9u8; hide::LEN_ASCON_128_KEY];
+    let mut ciphertext: [u8; hide::LEN_ASCON_128_CTXT] = [0u8; hide::LEN_ASCON_128_CTXT];
+    let mut plaintext: [u8; hide::LEN_ASCON_128_PTXT] = [0u8; hide::LEN_ASCON_128_PTXT];
+
+    board.send_host_debug(b"Testing Ascon-128!");
+    board.send_host_debug(b"Original message:");
+    board.send_host_debug(&message);
+
+    let result = hide::ascon_encrypt(
+        &mut ciphertext,
+        &message,
+        &associated_data,
+        &nonce,
+        &key,
+    );
+    if result != 0 {
+        panic!("Failed to encrypt message");
+    }
+    board.send_host_debug(b"Encrypted message:");
+    board.send_host_debug(&ciphertext);
+
+    let wrong_associated_data: [u8; hide::LEN_ASCON_128_AD] = [1, 2, 3, 4, 5, 6, 7, 9];
+    let result = hide::ascon_decrypt(
+        &mut plaintext,
+        &ciphertext,
+        &wrong_associated_data,
+        &nonce,
+        &key,
+    );
+    if result != 0 {
+        board.send_host_debug(b"Failed to decrypt message");
+    }
+
+    board.send_host_debug(b"Decrypted message:");
+    board.send_host_debug(&plaintext);
+
+    for i in 0..64 {
+        if message[i] != plaintext[i] {
+            board.send_host_debug(b"Plaintext does not match message");
+        }
     }
 }
