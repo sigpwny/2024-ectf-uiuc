b2aa2e5a517a0f172e3e5300a2a7d92de4821560
Shiv Gohil <sgohil3@illinois.edu>
2024-02-24 15:21:31 -0600

    added read function/ check rx/tx_busy and rx_empty fn, will check with team on compiling onto board

--- rust-lib/hal/src/uart0.rs
+++ rust-lib/hal/src/uart0.rs
@@ -2,15 +2,30 @@ pub use max78000_pac::UART;
 
 /// Send a byte over UART0
 pub fn send_byte(uart0: &UART, byte: u8) {
-    while is_tx_full(uart0) {}
+    //stall if tx is full or busy
+    while (is_tx_full(uart0) || is_tx_busy(uart0)) {}
     // Safety: The data field is 8 bits wide, which fits the byte
     uart0.fifo().write(|w| unsafe { w.data().bits(byte) });
 }
 
 /// Send bytes over UART0
 pub fn send_bytes(uart0: &UART, bytes: &[u8]) {
-    for byte in bytes {
+    for byte in bytes { //calls send_byte for num bytes will stall if full
         send_byte(uart0, *byte);
+    }
+}
+
+/// read a byte over UART0
+pub fn recieve_byte(uart0: &UART) {
+    //make sure that our rx is not busy/has data in it
+    while (is_rx_empty(uart0) || is_rx_busy(uart0)) {}
+    return uart0.fifo().read();
+}
+
+/// recieves bytes over UART0
+pub fn recieve_bytes(uart0: &UART, num_bytes: i32, bytes: &[u8]) {
+    for byte in bytes {
+        *byte = recieve_byte(uart0);
     }
 }
 
@@ -19,9 +34,24 @@ pub fn is_tx_full(uart0: &UART) -> bool {
     return uart0.status().read().tx_full().bit_is_set();
 }
 
+/// Checks if the UART0 RX FIFO is busy
+pub fn is_tx_busy(uart0: &UART) -> bool {
+    return uart0.status().read().tx_busy().bit_is_set();
+}
+
 /// Checks if the UART0 RX FIFO is full
 pub fn is_rx_full(uart0: &UART) -> bool {
     return uart0.status().read().rx_full().bit_is_set();
+}
+
+/// Checks if the UART0 RX FIFO is empty
+pub fn is_rx_empty(uart0: &UART) -> bool {
+    return uart0.status().read().rx_em().bit_is_set();
+}
+
+/// Checks if the UART0 RX FIFO is busy
+pub fn is_rx_busy(uart0: &UART) -> bool {
+    return uart0.status().read().rx_busy().bit_is_set();
 }
 
 /// Configure UART0 for 115200 baud, 8N1 operation
