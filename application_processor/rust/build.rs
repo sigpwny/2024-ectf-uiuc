e62f59c129675a26ad444cbfce549fd0ab817ba1
Minh Duong <git@whitehoodhacker.net>
2024-02-29 02:54:38 -0600

    POST_BOOT is working, kinda

--- application_processor/rust/build.rs
+++ application_processor/rust/build.rs
@@ -6,11 +6,12 @@ fn main() {
     let current_dir = std::env::current_dir().unwrap();
     let parent_dir = current_dir.parent().unwrap();
     let build_dir = parent_dir.join("c").join("build");
-    let lib_path = build_dir.join("libpostboot.a");
-    if !lib_path.exists() {
-        panic!("libpostboot.a does not exist! Please build the C code first.");
-    }
+    // let lib_path = build_dir.join("libpostboot.a");
+    // if !lib_path.exists() {
+    //     panic!("libpostboot.a does not exist! Please build the C code first.");
+    // }
     println!("cargo:rustc-link-search={}", build_dir.display());
+    // println!("cargo:rustc-link-lib=dylib=postbootandlibc");
     println!("cargo:rustc-link-lib=static=postboot");
     // println!("cargo:rustc-link-lib=static=msdk-periph");
     // println!("cargo:rustc-link-lib=static=msdk-board");
@@ -21,13 +22,13 @@ fn main() {
     println!("cargo:rerun-if-changed=NULL");
     // Link ARM Embedded GCC standard C library
     // the path is stored in environment variable GCC_ARM_PATH
-    let gcc_arm_path = env::var("GCC_ARM_PATH").unwrap();
-    let libc_path = PathBuf::from(gcc_arm_path).join("arm-none-eabi").join("lib").join("thumb").join("v7e-m+fp").join("softfp");
-    let libc_lib_path = libc_path.join("libc.a");
+    // let gcc_arm_path = env::var("GCC_ARM_PATH").unwrap();
+    // let libc_path = PathBuf::from(gcc_arm_path).join("arm-none-eabi").join("lib").join("thumb").join("v7e-m+fp").join("softfp");
+    // let libc_lib_path = libc_path.join("libc.a");
     // println!("cargo:rerun-if-changed={}", libc_lib_path.display());
-    if !libc_lib_path.exists() {
-        panic!("libc.a does not exist! Please set the GCC_ARM_PATH environment variable.");
-    }
+    // if !libc_lib_path.exists() {
+    //     panic!("libc.a does not exist! Please set the GCC_ARM_PATH environment variable.");
+    // }
     // println!("cargo:rustc-link-search={}", libc_path.display());
     // println!("cargo:rustc-link-lib=static=c");
 }
--- application_processor/rust/src/main.rs
+++ application_processor/rust/src/main.rs
@@ -69,3 +69,36 @@ fn main() -> ! {
         continue;
     }
 }
+
+#[no_mangle]
+pub extern "C" fn _sbrk() {}
+
+#[no_mangle]
+pub extern "C" fn _write() {}
+
+#[no_mangle]
+pub extern "C" fn _close() {}
+
+#[no_mangle]
+pub extern "C" fn _lseek() {}
+
+#[no_mangle]
+pub extern "C" fn _read() {}
+
+#[no_mangle]
+pub extern "C" fn _fstat() {}
+
+#[no_mangle]
+pub extern "C" fn _isatty() {}
+
+#[no_mangle]
+pub extern "C" fn _exit() {}
+
+#[no_mangle]
+pub extern "C" fn _open() {}
+
+#[no_mangle]
+pub extern "C" fn _kill() {}
+
+#[no_mangle]
+pub extern "C" fn _getpid() {}
