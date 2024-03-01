b4b72295d03e7480e1a5f0f01c00f5db9db205f2
Minh Duong <github@whitehoodhacker.net>
2024-03-01 17:53:37 -0600

    Add function signatures for secure send and secure receive

--- component/rust/src/main.rs
+++ component/rust/src/main.rs
@@ -6,12 +6,6 @@ use max78000_hal::tmr0;
 use board::{Board, Led, u8_to_hex_string, u32_to_hex_string};
 use board::secure_comms as hide;
 
-mod ectf_global_secrets;
-use ectf_global_secrets::{
-    ASCON_SECRET_KEY_AP_TO_C,
-    ASCON_SECRET_KEY_C_TO_AP,
-};
-
 mod ectf_params;
 use ectf_params::{
     COMPONENT_ID,
