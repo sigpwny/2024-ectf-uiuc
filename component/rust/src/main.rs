b75d4ef80755c6865a879c42f79eb507112a4d1b
Minh Duong <github@whitehoodhacker.net>
2024-03-01 17:41:54 -0600

    Add component params to Rust file

--- component/rust/src/main.rs
+++ component/rust/src/main.rs
@@ -1,8 +1,25 @@
 #![no_std]
 #![no_main]
 
-use crate::*;
 use cortex_m_rt::entry;
+use max78000_hal::tmr0;
+use board::{Board, Led, u8_to_hex_string, u32_to_hex_string};
+use board::secure_comms as hide;
+
+mod ectf_global_secrets;
+use ectf_global_secrets::{
+    ASCON_SECRET_KEY_AP_TO_C,
+    ASCON_SECRET_KEY_C_TO_AP,
+};
+
+mod ectf_params;
+use ectf_params::{
+    COMPONENT_ID,
+    COMPONENT_BOOT_MSG,
+    ATTESTATION_LOCATION,
+    ATTESTATION_DATE,
+    ATTESTATION_CUSTOMER,
+};
 
 
 #[entry]
