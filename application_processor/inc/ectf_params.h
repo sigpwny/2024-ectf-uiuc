f0712cdc3c0ae53712d30246410e505df8f0cfcd
Minh Duong <github@whitehoodhacker.net>
2024-03-02 16:28:35 -0600

    Delete application_processor/inc/ectf_params.h

--- application_processor/inc/ectf_params.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ECTF_PARAMS__
-#define __ECTF_PARAMS__
-#define AP_PIN "123456"
-#define AP_TOKEN "0123456789abcdef"
-#define COMPONENT_IDS 0x11111124, 0x11111125
-#define COMPONENT_CNT 2
-#define AP_BOOT_MSG "Test boot message"
-#endif
