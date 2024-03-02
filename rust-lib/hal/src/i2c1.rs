24ba8b19cdaa65767e92fd6773e393400b0601d2
Minh Duong <git@whitehoodhacker.net>
2024-03-02 07:40:24 -0600

    I2C cleanup, still strictly synchronous

--- rust-lib/hal/src/i2c1.rs
+++ rust-lib/hal/src/i2c1.rs
@@ -1,26 +1,14 @@
 pub use max78000_pac::I2C1;
 
 pub enum MasterI2CStatus {
-    Success,
-    Nack,
-    TxLockout,
-    ArbitrationLost,
-    InvalidState
+    Success = 0,
+    Nack = -1,
+    InvalidState = -2,
+    ArbitrationLost = -3,
 }
 
-pub enum SlaveI2CStatus {
-    Success,
-    Nack,
-    TxLockout,
-    InvalidState
-}
-
-/// Initialize I2C1
+/// Configure I2C master
 pub fn master_config(i2c1: &I2C1) {
-    // Recover func call & check return
-    // Disable I2C peripheral
-    // i2c1.ctrl().modify(|_, w| w.en().clear_bit());
-    // while i2c1.ctrl().read().en().bit_is_set() { }
     // Enable I2C peripheral
     i2c1.ctrl().modify(|_,w| w.en().set_bit());
     while i2c1.ctrl().read().en().bit_is_clear() { }
@@ -32,16 +20,11 @@ pub fn master_config(i2c1: &I2C1) {
     clear_rxfifo(&i2c1);
     // Set frequency
     set_frequency(&i2c1, 100_000);
-    // Set thresholds
-    // set_tx_threshold(&i2c1, 2);
-    // set_rx_threshold(&i2c1, 6);
-    // How do we want to do error return?
 }
 
-/// Configure I2C slave for just-in-time configuration
-/// Safety: I2C should be disabled before calling this function
-pub fn slave_config(i2c1: &I2C1, slave_addr: u8) {
-    let addr: u8 = slave_addr & 0x7F;
+/// Configure I2C slave
+pub fn slave_config(i2c1: &I2C1, addr: u8) {
+    let addr: u8 = addr & 0x7F;
     // Disable I2C peripheral
     i2c1.ctrl().modify(|_, w| w.en().clear_bit());
     while i2c1.ctrl().read().en().bit_is_set() { }
@@ -76,20 +59,11 @@ pub fn slave_config(i2c1: &I2C1, slave_addr: u8) {
 
 /// Set I2C frequency
 fn set_frequency(i2c1: &I2C1, hz: u32){
-    let mut ticksTotal = 0u16;
-    let mut hiClks = 0u16;
-    let mut lowClks = 0u16;
-
-    ticksTotal = (50000000 / hz) as u16;
-    hiClks = (ticksTotal >> 1) - 1;
-    lowClks = (ticksTotal >> 1) - 1;
-
-    i2c1.clklo().modify( unsafe{|_,w| w.lo().bits(lowClks)});
-    i2c1.clkhi().modify( unsafe{|_,w| w.hi().bits(hiClks)});
-}
-
-fn i2c_recover(i2c1: &I2C1, retires: u32){
-
+    let ticks_total: u16 = (50_000_000 / hz) as u16;
+    let hi_clks: u16 = (ticks_total >> 1) - 1;
+    let low_clks: u16 = (ticks_total >> 1) - 1;
+    i2c1.clkhi().modify( unsafe{|_,w| w.hi().bits(hi_clks)});
+    i2c1.clklo().modify( unsafe{|_,w| w.lo().bits(low_clks)});
 }
 
 /// Clear TX FIFO
@@ -97,164 +71,150 @@ fn clear_txfifo(i2c1: &I2C1){
     i2c1.txctrl0().modify(|_,w| w.flush().flush());
     while i2c1.txctrl0().read().flush().is_flush() {}
 }
-
 /// Clear RX FIFO
 fn clear_rxfifo(i2c1: &I2C1){
     i2c1.rxctrl0().modify(|_,w| w.flush().flush());
     while i2c1.rxctrl0().read().flush().is_flush() {}
 }
-
 /// Check if RX FIFO is empty
 fn rx_em(i2c1: &I2C1) -> bool {
     return i2c1.status().read().rx_em().bit_is_set();
 }
-
 /// Check if RX FIFO is full
 fn rx_full(i2c1: &I2C1) -> bool {
     return i2c1.status().read().rx_full().bit_is_set();
 }
-
 /// Check if TX FIFO is empty
 fn tx_em(i2c1: &I2C1) -> bool {
     return i2c1.status().read().tx_em().bit_is_set();
 }
-
 /// Check if TX FIFO is full
 fn tx_full(i2c1: &I2C1) -> bool {
     return i2c1.status().read().tx_full().bit_is_set();
 }
-
-/// Check if TX lockout is set
-fn is_tx_lockout(i2c1: &I2C1) -> bool {
-    return i2c1.intfl0().read().tx_lockout().bit_is_set();
-}
-
 /// Handle TX lockout
 fn handle_tx_lockout(i2c1: &I2C1) {
-    if is_tx_lockout(i2c1) {
+    if i2c1.intfl0().read().tx_lockout().bit_is_set() {
         i2c1.intfl0().modify(|_, w| w.tx_lockout().set_bit());
     }
 }
 
-/// Read data from a slave
+/// Read data from a slave.
+/// The component MUST be waiting for the read. If the component does not 
+/// send an ack, this function will spin indefinitely. This function blocks 
+/// until all the data is received.
 pub fn master_read_bytes(i2c1: &I2C1, addr: u8, bytes: &mut [u8]) -> MasterI2CStatus {
     // Make sure read bit is set
     let addr = (addr << 1) | 0x1;
     let len = bytes.len() as u8;
-    // The rx fifo should be empty when we call this function
-    if !rx_em(i2c1) {
-        return MasterI2CStatus::InvalidState;
-    }
     // Handle TX lockout
     handle_tx_lockout(i2c1);
+    // Reset state
+    clear_rxfifo(i2c1);
+    i2c1.intfl0().modify(|_, w| w
+        .addr_ack().set_bit()
+        .addr_nack_err().set_bit()
+    );
+    while i2c1.intfl0().read().addr_ack().bit_is_set() ||
+          i2c1.intfl0().read().addr_nack_err().bit_is_set() { }
     // Write number of expected bytes
     i2c1.rxctrl1().modify(|_, w| unsafe { w.cnt().bits(len) });
     // Write the address byte of the slave
     i2c1.fifo().modify(|_, w| unsafe { w.data().bits(addr) });
     // Write the start bit
     i2c1.mstctrl().modify(|_, w| w.start().set_bit() );
-
-//    // Wait until we either receive an ACK or NACK
-//    while
-//        i2c1.intfl0().read().addr_ack().bit_is_clear() ||
-//        i2c1.intfl0().read().addr_nack_err().bit_is_clear() { }
-//    // If we received a NACK, we should stop
-//    if i2c1.intfl0().read().addr_nack_err().bit_is_set() {
-//        i2c1.mstctrl().modify(|_, w| w.stop().set_bit());
-//        return MasterI2CStatus::Nack;
-//    }
-
+    // // Wait until we either receive an ACK or NACK
+    // while i2c1.intfl0().read().addr_ack().bit_is_clear() ||
+    //       i2c1.intfl0().read().addr_nack_err().bit_is_clear() { }
+    // // If we received a NACK, we should stop
+    // if i2c1.intfl0().read().addr_nack_err().bit_is_set() {
+    //     i2c1.intfl0().modify(|_, w| w.addr_nack_err().set_bit());
+    //     i2c1.mstctrl().modify(|_, w| w.stop().set_bit());
+    //     return MasterI2CStatus::Nack;
+    // }
+    // i2c1.intfl0().modify(|_, w| w.addr_ack().set_bit());
     // Read in bytes
     for byte in bytes.iter_mut() {
         while rx_em(i2c1) { }
         *byte = i2c1.fifo().read().data().bits();
     }
-    // Handle transaction done interrupt flag
-    if i2c1.intfl0().read().done().bit_is_set() {
-        i2c1.intfl0().modify(|_, w| w.done().set_bit());
-        i2c1.mstctrl().modify(|_, w| unsafe { w.stop().bit(true)});
-    } else {
-        return MasterI2CStatus::InvalidState;
-    }
+    // Set stop bit
+    i2c1.mstctrl().modify(|_, w| w.stop().set_bit());
     return MasterI2CStatus::Success;
 }
 
-
-pub fn master_write_bytes(i2c1: &I2C1, addr: u8, data: &[u8]) {
+/// Write data to a slave.
+/// The slave must be waiting for the write. If the slave is not waiting and 
+/// this function is called, this will spin indefinitely.
+pub fn master_write_bytes(i2c1: &I2C1, addr: u8, bytes: &[u8]) -> MasterI2CStatus {
+    // Make sure read bit is not set
     let addr = (addr << 1) & 0xFE;
-    let mut written = 0usize;
-    let mut started = false;
-
-    while written < data.len() + 1 {
-       
-        while !tx_em(i2c1) {}
-
-        if !started {
-            i2c1.fifo().modify(|_, w| unsafe { w.data().bits(addr) });
-            written += 1;
-        }
-        
-        while !tx_full(i2c1) && written < data.len() + 1 {
-            i2c1.fifo().modify(|_, w| unsafe { w.data().bits(data[written - 1]) });
-            written += 1;
-        }
-
-        // set master control start to 1
-        if !started {
-            i2c1.mstctrl().modify(|_, w| unsafe { w.start().bit(true)});
-            started = true;
-        }
-
+    // Handle TX lockout
+    handle_tx_lockout(i2c1);
+    // Reset state
+    clear_txfifo(i2c1);
+    i2c1.intfl0().modify(|_, w| w
+        .addr_ack().set_bit()
+        .addr_nack_err().set_bit()
+    );
+    while i2c1.intfl0().read().addr_ack().bit_is_set() ||
+          i2c1.intfl0().read().addr_nack_err().bit_is_set() { }
+    // Write the address byte of the slave
+    i2c1.fifo().modify(|_, w| unsafe { w.data().bits(addr) });
+    // Set start bit
+    i2c1.mstctrl().modify(|_, w| w.start().set_bit());
+    // Wait until we either receive an ACK or NACK
+    // while
+    //     i2c1.intfl0().read().addr_ack().bit_is_clear() ||
+    //     i2c1.intfl0().read().addr_nack_err().bit_is_clear() { }
+    // // If we received a NACK, we should stop
+    // if i2c1.intfl0().read().addr_nack_err().bit_is_set() {
+    //     i2c1.intfl0().modify(|_, w| w.addr_nack_err().set_bit());
+    //     i2c1.mstctrl().modify(|_, w| w.stop().set_bit());
+    //     return MasterI2CStatus::Nack;
+    // }
+    // i2c1.intfl0().modify(|_, w| w.addr_ack().set_bit());
+    // Write bytes
+    for byte in bytes {
+        while tx_full(i2c1) { }
+        i2c1.fifo().modify(|_, w| unsafe { w.data().bits(*byte) });
     }
-    i2c1.mstctrl().modify(|_, w| unsafe { w.stop().bit(true)});
+    // Set stop bit
+    i2c1.mstctrl().modify(|_, w| w.stop().set_bit());
+    // Wait until the last byte is sent
+    while !tx_em(i2c1) { }
+    return MasterI2CStatus::Success;
 }
 
-/// Read data from master
-pub fn slave_read_bytes(i2c1: &I2C1, bytes: &mut [u8]) -> SlaveI2CStatus {
+/// Read data from master.
+/// This function will block until the master sends all the data.
+pub fn slave_read_bytes(i2c1: &I2C1, bytes: &mut [u8]) {
     // Wait until addressed by master
     while !i2c1.intfl0().read().rd_addr_match().bit_is_set() { }
-    
-
-    i2c1.intfl0().modify(|_, w| w.addr_match().clear_bit());
+    i2c1.intfl0().modify(|_, w| w.rd_addr_match().clear_bit());
+    // Reset state
+    handle_tx_lockout(i2c1);
     // Read byte from FIFO
     for byte in bytes.iter_mut() {
         while rx_em(i2c1) { }
         *byte = i2c1.fifo().read().data().bits();
     }
-    // Check for invalid state
-    if !rx_em(i2c1) ||
-        i2c1.intfl1().read().rx_ov().bit_is_set() ||
-        !i2c1.intfl0().read().done().bit_is_set()
-    {
-        clear_rxfifo(i2c1);
-        return SlaveI2CStatus::InvalidState;
-    }
-    i2c1.intfl0().modify(|_, w| w.done().set_bit());
-    return SlaveI2CStatus::Success;
 }
 
-/// Write data to master (when prompted)
-pub fn slave_write_bytes(i2c1: &I2C1, bytes: &[u8]) -> SlaveI2CStatus {
+/// Write data to master (when prompted).
+/// This function will block until the master reads all the data.
+pub fn slave_write_bytes(i2c1: &I2C1, bytes: &[u8]) {
     // Wait until addressed by master
     while !i2c1.intfl0().read().wr_addr_match().bit_is_set() { }
-    // Ensure master requested a write
-
-    i2c1.intfl0().modify(|_, w| w.addr_match().clear_bit());
-    // There shouldn't be anything in the TX FIFO
-    if !tx_em(i2c1) {
-        return SlaveI2CStatus::InvalidState;
-    }
-    // Handle TX lockout
+    i2c1.intfl0().modify(|_, w| w.wr_addr_match().clear_bit());
+    // Reset state
     handle_tx_lockout(i2c1);
+    clear_txfifo(i2c1);
     // Write bytes to FIFO
     for byte in bytes {
         while tx_full(i2c1) { }
-        i2c1.fifo().modify(|_, w| unsafe { w.data().bits(*byte) });
+        i2c1.fifo().modify(|_, w| unsafe {
+            w.data().bits(*byte)
+        });
     }
-    // Check for invalid state
-    if !i2c1.intfl0().read().done().bit_is_set() {
-        return SlaveI2CStatus::InvalidState;
-    }
-    i2c1.intfl0().modify(|_, w| w.done().set_bit());
-    return SlaveI2CStatus::Success;
 }
--- rust-lib/max78000-test/src/main.rs
+++ rust-lib/max78000-test/src/main.rs
@@ -16,30 +16,97 @@ fn main() -> ! {
     //test_ascon(&board);
     //test_random(&board);
 
-    loop {
-        let mut data: [u8; 4] = [b'a', b'b', b'a', b'\n'];
-        i2c1::master_write_bytes(&board.i2c1, 0x0b, &data);
-        for _ in 0..1000 {
-            cortex_m::asm::nop();
-        }
-    }
+    test_i2c(&board);
 
     loop {
         // This timer logic doesn't make any sense, don't use it
-        let tmr_cnt = tmr0::get_tick_count(&board.tmr0);
-        while tmr0::get_tick_count(&board.tmr0) < tmr_cnt + 50_000_000 { }
-        board.send_host_debug(b"Hello, world!");
-        if count % 2 == 0 {
-            board.led_on(Led::Green);
-        } else {
-            board.led_off(Led::Green);
-        }
-        // Test panic
-        if count == 20 {
-            panic!("Panicked after 20 seconds!");
-        }
-        count += 1;
+        // let tmr_cnt = tmr0::get_tick_count(&board.tmr0);
+        // while tmr0::get_tick_count(&board.tmr0) < tmr_cnt + 50_000_000 { }
+        // board.send_host_debug(b"Hello, world!");
+        // if count % 2 == 0 {
+        //     board.led_on(Led::Green);
+        // } else {
+        //     board.led_off(Led::Green);
+        // }
+        // // Test panic
+        // if count == 20 {
+        //     panic!("Panicked after 20 seconds!");
+        // }
+        // count += 1;
         continue;
+    }
+}
+
+fn test_i2c(board: &Board) {
+    let master = true;
+    const LEN_MSG_1: usize = 25;
+    const LEN_MSG_2: usize = 17;
+    const SLAVE_ADDR: u8 = 0x0b;
+    // Master test
+    if master {
+        i2c1::master_config(&board.i2c1);
+        let mut sent = false;
+        // Send message to slave
+        while !sent {
+            let data: &[u8; LEN_MSG_1] = b"This is more than 4 bytes";
+            let result = i2c1::master_write_bytes(&board.i2c1, SLAVE_ADDR, data);
+            match result {
+                i2c1::MasterI2CStatus::Success => {
+                    board.send_host_debug(b"[Info] Data sent successfully!");
+                    sent = true;
+                },
+                i2c1::MasterI2CStatus::Nack => {
+                    board.send_host_debug(b"[Info] Nack received, retrying...");
+                    for _ in 0..50_000_000 {
+                        cortex_m::asm::nop();
+                    }
+                },
+                _ => {
+                    board.send_host_debug(b"[Info] Unknown error, retrying...");
+                    for _ in 0..50_000_000 {
+                        cortex_m::asm::nop();
+                    }
+                },
+            }
+        }
+        // board.led_on(Led::Green);
+        // Read message from slave
+        let mut data: [u8; LEN_MSG_2] = [0; LEN_MSG_2];
+        let mut received = false;
+        while !received {
+            let result = i2c1::master_read_bytes(&board.i2c1, SLAVE_ADDR, &mut data);
+            match result {
+                i2c1::MasterI2CStatus::Success => {
+                    board.send_host_debug(b"[Info] Data received from slave:");
+                    board.send_host_debug(&data);
+                    received = true;
+                },
+                i2c1::MasterI2CStatus::Nack => {
+                    board.send_host_debug(b"[Info] Nack received, retrying...");
+                    for _ in 0..50_000_000 {
+                        cortex_m::asm::nop();
+                    }
+                },
+                _ => {
+                    board.send_host_debug(b"[Info] Unknown error, retrying...");
+                    for _ in 0..50_000_000 {
+                        cortex_m::asm::nop();
+                    }
+                },
+            }
+        }
+        // board.led_on(Led::Blue);
+    // Slave test
+    } else {
+        i2c1::slave_config(&board.i2c1, SLAVE_ADDR);
+        let mut data: [u8; LEN_MSG_1] = [0; LEN_MSG_1];
+        i2c1::slave_read_bytes(&board.i2c1, &mut data);
+        board.send_host_debug(b"[Info] Data received from master:");
+        board.send_host_debug(&data);
+        // board.led_on(Led::Green);
+        let msg2: &[u8; LEN_MSG_2] = b"Hello from slave!";
+        i2c1::slave_write_bytes(&board.i2c1, msg2);
+        // board.led_on(Led::Blue);
     }
 }
 
