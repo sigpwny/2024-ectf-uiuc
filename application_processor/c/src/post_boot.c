3cc22034724e44e1073aa83ff94aa78a6fed90d3
Minh Duong <git@whitehoodhacker.net>
2024-03-05 02:20:21 -0600

    Fix post_boot default code

--- application_processor/c/src/post_boot.c
+++ application_processor/c/src/post_boot.c
@@ -16,55 +16,22 @@ void post_boot() {
 #ifdef POST_BOOT
     POST_BOOT
 #else
-    // Application Processor
-    uint8_t post_boot_buffer[256];
-    printf("Post boot: Insulin Pump started!\n ");
-    uint32_t post_boot_component_ids[10];
-    int post_boot_component_cnt;
-    post_boot_component_cnt = get_provisioned_ids(post_boot_component_ids);
-    uint8_t post_boot_sensor_addr = 0;
-    uint8_t post_boot_actuator_addr = 0;
-    for (int i = 0; i < post_boot_component_cnt; i++) {
-        uint8_t addr = (uint8_t)(post_boot_component_ids[i] & 0xFF);
-        printf("Found address: %d\n", addr);
-        post_boot_buffer[0] = 0;
-        secure_send(addr, post_boot_buffer, 1);
-        secure_receive(addr, post_boot_buffer);
-        switch (post_boot_buffer[0]) {
-            case 0:
-                printf("Found address for sensor: %d\n", addr);
-                post_boot_sensor_addr = addr;
-                break;
-            case 1:
-                printf("Found address for actuator: %d\n", addr);
-                post_boot_actuator_addr = addr;
-                break;
-        }
-    }
-    uint32_t sensor_values[5] = {0, 0, 0, 0, 0};
-    int array_index = 0;
-    while (true) {
-        post_boot_buffer[0] = 1;
-        secure_send(post_boot_sensor_addr, post_boot_buffer, 1);
-        secure_receive(post_boot_sensor_addr, post_boot_buffer);
-        sensor_values[array_index] = *(uint32_t*)post_boot_buffer;
-        array_index = (array_index + 1) % 5;
-        uint32_t sensor_sum = 0;
-        for (int i = 0; i < 5; i++) {
-            sensor_sum += sensor_values[i];
-        }
-        float sensor_avg = ((float)sensor_sum) / 5.0;
-        if (sensor_avg > 128.0) {
-            post_boot_buffer[0] = 1;
-            post_boot_buffer[1] = 1;
-            secure_send(post_boot_actuator_addr, post_boot_buffer, 2);
-            secure_receive(post_boot_actuator_addr, post_boot_buffer);
-            printf("%%success: %s\n%%", post_boot_buffer);
-        } else {
-            post_boot_buffer[0] = 1;
-            post_boot_buffer[1] = 0;
-            secure_send(post_boot_actuator_addr, post_boot_buffer, 2);
-        }
+    int number = 0;
+    while (1) {
+        printf("Hello from AP POST_BOOT! %d\n", number);
+        fflush(stdout);
+        number++;
+        LED_On(LED1);
+        MXC_Delay(500000);
+        LED_On(LED2);
+        MXC_Delay(500000);
+        LED_On(LED3);
+        MXC_Delay(500000);
+        LED_Off(LED1);
+        MXC_Delay(500000);
+        LED_Off(LED2);
+        MXC_Delay(500000);
+        LED_Off(LED3);
         MXC_Delay(500000);
     }
 #endif
--- component/c/src/post_boot.c
+++ component/c/src/post_boot.c
@@ -16,50 +16,23 @@ void post_boot() {
 #ifdef POST_BOOT
     POST_BOOT
 #else
-#ifdef SENSOR
-    uint8_t post_boot_buffer[256];
-    int array_index = 0;
-    uint32_t sensor_values[10] = {150, 150, 150, 150, 150, 150, 150, 150, 150, 150};
-    while (true) {
-        secure_receive(post_boot_buffer);
-        switch (post_boot_buffer[0]) {
-            case 0:
-            post_boot_buffer[0] = 0;
-            secure_send(post_boot_buffer, 1);
-            break;
-            case 1:
-            *(uint32_t*)post_boot_buffer = sensor_values[array_index];
-            secure_send(post_boot_buffer, sizeof(uint32_t));
-            array_index = (array_index + 1) % 10;
-            break;
-        }
+    int number = 0;
+    while (1) {
+        printf("Hello from Component POST_BOOT! %d\n", number);
+        fflush(stdout);
+        number++;
+        LED_On(LED1);
+        MXC_Delay(500000);
+        LED_On(LED2);
+        MXC_Delay(500000);
+        LED_On(LED3);
+        MXC_Delay(500000);
+        LED_Off(LED1);
+        MXC_Delay(500000);
+        LED_Off(LED2);
+        MXC_Delay(500000);
+        LED_Off(LED3);
+        MXC_Delay(500000);
     }
-#else
-    // Component 2 - Actuator
-    uint8_t post_boot_buffer[256];
-    while (true) {
-        secure_receive(post_boot_buffer);
-        switch (post_boot_buffer[0]) {
-        case 0:
-            post_boot_buffer[0] = 1;
-            secure_send(post_boot_buffer, 1);
-            break;
-        case 1:
-            if (post_boot_buffer[1] == 1) {
-                LED_On(LED1);
-                LED_On(LED2);
-                LED_On(LED3);
-                const char * post_boot_flag = "17c09d07e508e71eaf7bb98181b426ac8180a3139dc84f5bc94e0d8a1056707";
-                strcpy((char*)post_boot_buffer, post_boot_flag);
-                secure_send(post_boot_buffer, strlen(post_boot_flag) + 1);
-            } else {
-                LED_Off(LED1);
-                LED_Off(LED2);
-                LED_Off(LED3);
-            }
-            break;
-        }
-    }
-#endif
 #endif
 }
