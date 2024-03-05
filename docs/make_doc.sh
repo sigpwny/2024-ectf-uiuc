3e373bfe06ac70e53caae7dea26f638e12781ca2
Minh Duong <git@whitehoodhacker.net>
2024-03-05 04:27:15 -0600

    Add README, delete flash.md

--- docs/make_doc.sh
+++ docs/make_doc.sh
@@ -1,1 +1,1 @@
-pandoc --number-sections hide_protocol.md misc_protocol.md flash.md -F mermaid-filter -o UIUC_Documentation.pdf --pdf-engine=tectonic
+pandoc --number-sections hide_protocol.md misc_protocol.md -F mermaid-filter -o 2024_eCTF_UIUC_Design_Document.pdf --pdf-engine=tectonic
